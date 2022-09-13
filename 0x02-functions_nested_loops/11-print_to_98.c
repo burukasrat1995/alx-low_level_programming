7232ba9d3b4bce7ea708abba6c1a3e4434723aef
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 12:49:33 +0000

    eskagegnesh

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <iostream.h>
+/**
+ * print_to_98 - prints from n to 98
+ * @n: value of n
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf("\n");
+}
