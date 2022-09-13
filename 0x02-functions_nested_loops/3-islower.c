23f2e1a4528002a2fedef0db61d098c363dc1937
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 11:44:20 +0000

    answers were given

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 /**
  * _islower - checks lowercase letters
  * Return: 1 if c is lowercase or 0 otherwise
+ * @c - is the ascii value of the character
  */
 int _islower(int c)
 {
-	if (c >= 122 && c <= 97)
+	if (c <= 122 && c >= 97)
 	{
 		return (1);
 	}
