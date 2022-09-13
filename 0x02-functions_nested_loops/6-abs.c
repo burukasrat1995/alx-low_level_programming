58d5089c7864b9f81c0540622ac445af5de2c04f
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 12:16:22 +0000

    some changes regarding variables

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,12 @@
 /**
  * _abs - absolute value of a number
  * Return: the absolute value
+ * @c: value of the number
  */
-int _abs(int)
+int _abs(int c)
 {
-	if (int < 0)
-		return (int*-1);
-	else if (int >= 0)
-		return (int);
+	if (c < 0)
+		return (c*-1);
+	else if (c >= 0)
+		return (c);
 }
