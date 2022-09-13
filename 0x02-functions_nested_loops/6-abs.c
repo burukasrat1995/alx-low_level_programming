a787e0ca683bf8a46d2b765156043082d133b8f4
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 12:14:34 +0000

    this is absolute value function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - absolute value of a number
+ * Return: the absolute value
+ */
+int _abs(int)
+{
+	if (int < 0)
+		return (int*-1);
+	else if (int >= 0)
+		return (int);
+}
