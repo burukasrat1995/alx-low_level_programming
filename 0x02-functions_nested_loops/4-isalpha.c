130cdcfe5e179bc87b0085e0f690ca1b9056bd85
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 11:51:22 +0000

    this could mean anyting

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * _isalpha - checks if it is alphabetic character
+ * @c: ascii value of the char
+ * Return: 1 if letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c <= 122 && c >= 97) || (c <= 90 && c >= 65))
+		return (1);
+	else
+		return (0);
+}
