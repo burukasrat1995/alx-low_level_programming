0d32ea97d21c6f6f24365fcb454691557995a51f
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 15:59:05 +0000

    worked finally

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,34 +6,33 @@
 void jack_bauer(void)
 {
 	int n1, n2, n3, n4, min = 0;
-
-		n1 = 48;
-		while (n1 < 51)
+	n1 = 48;
+	while (n1 < 51)
+	{
+		n2 = 48;
+		while (n2 < 58)
 		{
-			n2 = 48;
-			while (n2 < 58)
+			n3 = 48;
+			while (n3 < 54)
 			{
-				n3 = 48;
-				while (n3 < 54)
+				n4 = 48;
+				while (n4 < 58)
 				{
-					n4 = 48;
-					while (n4 < 58)
-					{
-						_putchar(n1);
-						_putchar(n2);
-						_putchar(58);
-						_putchar(n3);
-						_putchar(n4);
-						_putchar('\n');
-						n4++;
-						min++;
-						if (min == 1440)
-							return;
-					}
-					n3++;
+					_putchar(n1);
+					_putchar(n2);
+					_putchar(58);
+					_putchar(n3);
+					_putchar(n4);
+					_putchar('\n');
+					n4++;
+					min++;
+					if (min == 1440)
+						return;
 				}
-				n2++;
+				n3++;
 			}
-			n1++;
+			n2++;
 		}
+		n1++;
+	}
 }
