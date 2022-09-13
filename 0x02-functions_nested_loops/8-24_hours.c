fbc92f7db25fc3d7454da651bc1292b1dfe01ac8
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 15:33:38 +0000

    hardest

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minutes of a day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int min = 0;
+	int hour1 = 48;
+
+	while (hour1 < 51)
+	{
+		int hour2 = 48;
+
+		_putchar(hour1);
+		hour1++;
+		while (hour2 < 58)
+		{
+			int min1 = 48;
+
+			_putchar(hour2);
+			_putchar(59);
+			hour2++;
+			while (min1 < 54)
+			{
+				int min2 = 48;
+				_putchar(min1);
+				min1++;
+				while (min2 < 58)
+				{
+					_putchar(min2);
+					_putchar('\n');
+					min2++;
+					break;
+				}
+				break;
+			}
+			break;
+		}
+		break;
+	}
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void print_to_98(int n);
+void hack_bauer(void);
