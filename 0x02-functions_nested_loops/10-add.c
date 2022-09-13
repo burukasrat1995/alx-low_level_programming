5ca18ed30334f53d6f40d15c9e4fb52f21a9735e
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 16:12:40 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * Return: sum of a and b
+ * @a: num1
+ * @b: num2
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * times_table - print's the times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, col;
+
