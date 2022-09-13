77400dc4dce8f577c0557f015754a35f134a2cf7
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 17:34:54 +0000

    you broke me first

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,16 +8,19 @@ int main(void)
 	long int a = 1;
 	long int b = 2;
 	long int b1 = 0;
+	long int sum = 0;
 	int loop = 0;
 
-	while (loop < 49)
+	while (a < 4000000)
 	{
-		printf("%ld, ", a);
+		if (a % 2 == 0)
+			sum =+ a;
 		b1 = b;
 		b = a + b;
 		a = b1;
 		loop++;
 	}
-	printf("%ld\n", a);
+	printf("%ld\n", sum);
 	return (0);
 }
+
