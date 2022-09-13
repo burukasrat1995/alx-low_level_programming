b9f01fb68d3f795f4a9904ef13bcd38075340b7e
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 19:52:34 +0000

    dreamed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: void
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+	n = 'a';
+	while (n <= 'f')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return(0);
+}
