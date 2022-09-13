7e1402688e8adbe6f62cba18e86f03ebf8e66889
Buruk Asrat <burukasrat@gmail.com>
2022-09-13 11:22:52 +0000

    this is the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabe.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - main function
+ * Return: 0
+ */
+int main()
+{
+	int loop = 97;
+
+	while (loop < 123)
+	{
+		_putchar(loop);
+		loop++;
+	}
+	_putchar('\n');
+	return (0);
+}
