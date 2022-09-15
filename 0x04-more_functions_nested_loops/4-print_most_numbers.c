c323f31f05956167173f85e0979e148dcf5b1e2a
Buruk Asrat <burukasrat@gmail.com>
2022-09-15 09:09:24 +0000

    2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		if !( n == '2' || n == '4')
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
