eaad169e93e7cfd8208d3c080b35116abe18e0ab
Buruk Asrat <burukasrat@gmail.com>
2022-09-15 08:59:33 +0000

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: value of first integer
+ * @b: value of second integer
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
